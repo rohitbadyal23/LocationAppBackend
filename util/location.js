ad71d409de12cf8466578c8b23954e3cfe19a9a3
rohit gill <RohitBadyal@rohits-MacBook-Pro-2.local>
2020-07-28 15:01:24 -0400

    prepared for deployment

---
+++ util/location.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+const  HttpError = require('../models/http-error');
+
+const API_KEY = 'AIzaSyCjI1EhcO1LxOXWF9-x0rxyiddLq9u5nGs';
+
+async function getCoordsForAddress(address) {
+	// return {
+	// 	lat: 40.7484474,
+	// 	lng: -73.9871516
+	// };
+	const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`);
+
+	const data = response.data;
+
+	if(!data || data.status === 'ZERO_RESULTS') {
+		const error = new HttpError('Could not find location for the address', 422);
+		throw error;
+	}
+
+	const coordinates = data.results[0].geometry.location;
+
+	return coordinates;
+}
+
+module.exports = getCoordsForAddress
